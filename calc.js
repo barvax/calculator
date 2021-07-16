6d1a0059032ba73d1233432bc1d6029e699788d6
barvax <69000340+barvax@users.noreply.github.com>
2021-07-16 19:09:58 +0300

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,1 +1,39 @@
-console.log("hi");
+var num1 = 0;
+var num2 = 0;
+var oparator;
+var result;
+
+var screen = document.getElementById('input');
+
+function enterOnClick(button){
+
+    screen.value+=button;
+}
+function clearMe(){
+    screen.value="";
+}
+
+function myOparator(sign){
+num1 = Number(screen.value);
+oparator = sign;
+clearMe();
+}
+
+function calc(){
+    num2 = Number(screen.value);
+    if(oparator=="+"){
+       result =  num1+num2;
+       screen.value = result;
+       
+       
+    }else if(oparator=="-"){
+        result =  num1-num2;
+       screen.value = result;
+    }else if(oparator=="*"){
+        result =  num1*num2;
+        screen.value = result;
+    }else if(oparator=="/"){
+        result =  num1/num2;
+        screen.value = result;
+    }
+}
